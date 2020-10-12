b585cd5f82350ad9f91a2ec2b8d9046119c2e848
Shiro-13 <shirai0119@gmail.com>
2020-10-12 21:47:25 +0900

    6章ルーティング設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     @user = Hash.new
     @user[:name] = 'Shohei Aoki'
     @user[:username] = 'moyashima'
-    @user[:location] = 'TOttori, Japan'
+    @user[:location] = 'Tottori, Japan'
     @user[:about] = 'Nice to meet you.'
   end
 end
